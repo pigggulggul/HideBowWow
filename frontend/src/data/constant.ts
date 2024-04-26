924f309ecfc10e46c98688af66fc4271db46e3d1
지준영 <bigwkddo@naver.com>
2024-04-26 16:45:20 +0900

    Revert "Merge branch 'front/feat/#16-server-to-client-position' into 'develop'"
    This reverts merge request !24

--- frontend/src/data/constant.ts
+++
@@ -1,7 +0,0 @@
-export const groundMapSize = 50;
-
-export const STEPS = {
-    NICK_NAME: 0,
-    CHARACTER: 1,
-    FINISH: 2,
-};
--- frontend/src/util.ts
+++
@@ -1,3 +0,0 @@
-export const isValidText = (text: string) => {
-    return Boolean(text && text.trim() !== '');
-};
