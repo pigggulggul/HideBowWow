fcf7929d4819a22ea30b7586a4ee8c74f2276f7c
김종범 <kbumk1234@naver.com>
2024-04-29 18:01:47 +0900

    fix: fps 수정

--- backend/game_service/src/main/java/com/s10p31a709/game/api/commandCenter/CommandCenter.java
+++ backend/game_service/src/main/java/com/s10p31a709/game/api/commandCenter/CommandCenter.java
@@ -91,7 +91,7 @@ public class CommandCenter {
         }
     }
 
-    @Scheduled(fixedRate = 30)
+    @Scheduled(fixedRate = 70)
     public void positionSchedule(){
         List<Room> rooms = roomRepository.findAllRoom();
         for (Room room : rooms){
