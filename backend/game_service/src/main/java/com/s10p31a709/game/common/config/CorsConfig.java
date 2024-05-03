d2735a0d4a4dfcc9cfd73ae65c12226b9633d15c
김종범 <kbumk1234@naver.com>
2024-05-03 14:39:30 +0900

    fix: cors설정 변경

--- backend/game_service/src/main/java/com/s10p31a709/game/common/config/CorsConfig.java
+++ backend/game_service/src/main/java/com/s10p31a709/game/common/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOrigins("http://hidebowwow.site", "https://hidebowwow.site", "http://localhost:5173", "http://localhost:5174", "http://k10a709.p.ssafy.io", "https://k10a709.p.ssafy.io")
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(false)
