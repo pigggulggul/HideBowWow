25b61349a8013289562ea26107257013705122af
김종범 <kbumk1234@naver.com>
2024-05-13 14:37:55 +0900

    feat: 봇 삭제 안되는 현상 수정

--- backend/game_service/src/main/java/com/s10p31a709/game/api/socket/service/RoomSocketService.java
+++ backend/game_service/src/main/java/com/s10p31a709/game/api/socket/service/RoomSocketService.java
@@ -145,7 +145,7 @@ public class RoomSocketService {
 
         // 봇 삭제
         List<Player> players = room.getRoomPlayers();
-        players = players.stream().filter(player -> !player.getNickname().startsWith("Computer")).toList();
+        room.setRoomPlayers(players.stream().filter(player -> !player.getNickname().startsWith("Computer")).toList());
 
         StompPayload<Room> payload = new StompPayload<>("room.backRoom", roomId, "system", room);
         simpMessagingTemplate.convertAndSend("/sub/room/"+roomId, payload);
