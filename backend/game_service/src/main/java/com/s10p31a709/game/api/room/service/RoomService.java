7b47a1e08e7cb38fe5438b57e769c5e650a38ca4
김종범 <kbumk1234@naver.com>
2024-05-13 13:26:23 +0900

    feat: AI배치를 기존 players에 추가
    기존 player수 파악하는 로직에 AI가 포함되지 않도록 수정

--- backend/game_service/src/main/java/com/s10p31a709/game/api/room/service/RoomService.java
+++ backend/game_service/src/main/java/com/s10p31a709/game/api/room/service/RoomService.java
@@ -37,13 +37,14 @@ public class RoomService {
         room.setRoomPlayers(new CopyOnWriteArrayList<>());
         room.setRoomState(0);
         room.setRoomTime(0);
+        room.setRoomMap("richRoom");
         return roomRepository.saveRoom(room);
     }
 
     public void checkEnter(String roomId, String password, String nickname){
         Room room = roomRepository.findRoomByRoomId(roomId);
         if(room == null) throw new CustomException(404, "방이 존재하지 않습니다");
-        if(room.getRoomPlayers().size() >= gameProperties.getMaxCapacity()) throw new CustomException(400, "방이 가득 찼습니다.");
+        if(room.getRoomPlayers().stream().filter(player -> !player.getNickname().startsWith("Computer")).toList().size() >= gameProperties.getMaxCapacity()) throw new CustomException(400, "방이 가득 찼습니다.");
         if(!room.getRoomState().equals(0)) throw new CustomException(400, "게임이 진행중인 방 입니다");
         if(!room.getIsPublic() && !room.getRoomPassword().equals(password)) throw new CustomException(400, "비밀번호가 틀렸습니다.");
     }
--- backend/member_service/src/main/java/com/s10p31a709/member/service/MemberService.java
+++ backend/member_service/src/main/java/com/s10p31a709/member/service/MemberService.java
@@ -82,7 +82,7 @@ public class MemberService {
 
             LocalDateTime now = LocalDateTime.now();
             Duration duration = Duration.between(now, member.getUpdatedTime());
-            if (Math.abs(duration.getSeconds()) > 5){
+            if (Math.abs(duration.getSeconds()) > 10){
                 deleteGuest(member.getNickname());
                 log.info("heartbeat 시간만료: {}", member);
 
