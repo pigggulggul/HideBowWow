5f63d5cfde33f376694cee4172108f03a29b8a9d
지준영 <bigwkddo@naver.com>
2024-05-16 14:36:45 +0900

    fix : 초반 관전 자막 버그 해결

--- frontend/src/components/content/canvas/maps/player/hooks/useObject.ts
+++ frontend/src/components/content/canvas/maps/player/hooks/useObject.ts
@@ -848,7 +848,7 @@ export const useObject = ({ player, position, modelIndex }: PlayerInitType) => {
         return () => {
             document.removeEventListener('keydown', handleJumpDown);
         };
-    }, [isJumping, chatFlag]);
+    }, [isJumping, chatFlag]); 
 
     useFrame(({ camera, clock }) => {
         if (!player || !playerRef.current) return;
